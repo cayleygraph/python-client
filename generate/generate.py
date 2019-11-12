0989aa48a46bfbcbf26bfb7225daab665b4bc6a3
Iddan Aaronsohn <mail@aniddan.com>
2019-11-12 02:32:59 +0200

    Use ast for code generation

--- generate/generate.py
+++ generate/generate.py
@@ -1,5 +1,8 @@
+import ast
+import astor
 import json
 from pathlib import Path
+from typing import Dict, List
 from . import snake_case
 
 directory = Path(__file__).parent
@@ -30,21 +33,23 @@ def remove_linked_ql(name):
     return name.replace("linkedql:", "")
 
 
-def range_to_type(_range):
+def range_to_type(_range) -> ast.expr:
     if _range["@id"] in {"linkedql:ValueStep", "linkedql:Step"}:
-        return "'Path'"
+        return ast.Str(s="Path")
     if _range["@id"] == "xsd:string":
-        return "str"
+        return ast.Name(id="str")
     if _range["@id"] == "xsd:int":
-        return "int"
+        return ast.Name(id="int")
     if _range["@id"] == "xsd:float":
-        return "float"
+        return ast.Name(id="float")
     if _range["@id"] == "xsd:boolean":
-        return "bool"
+        return ast.Name(id="bool")
     if _range["@id"] == "linkedql:Operator":
-        return "Operator"
+        return ast.Name(id="Operator")
     if _range["@id"] == "rdfs:Resource":
-        return "rdflib.term.Node"
+        return ast.Attribute(
+            value=ast.Attribute(value=ast.Name(id="rdflib"), attr="term"), attr="Node"
+        )
     raise Exception(f"Unexpected range: {_range}")
 
 
@@ -55,14 +60,17 @@ def normalize_keywords(name):
 
 
 def generate() -> str:
-    with header_path.open() as file:
-        text = file.read()
+    module = astor.code_to_ast.parse_file(header_path)
+    # print(astor.dump_tree(tree))
     with schema_path.open() as file:
         schema = json.load(file)
 
-    step_classes = []
-    restrictions = {}
-    properties_by_domain = {}
+    class_def = module.body[-1]
+    assert isinstance(class_def, ast.ClassDef) and class_def.name == "Path"
+
+    step_classes: List[dict] = []
+    restrictions: Dict[str, dict] = {}
+    properties_by_domain: Dict[str, List[dict]] = {}
 
     for document in schema:
         if is_restriction(document):
@@ -86,33 +94,49 @@ def generate() -> str:
         method_name = normalize_keywords(
             snake_case.convert(remove_linked_ql(step_class["@id"]))
         )
-        method_str = f"def {method_name}("
-        arguments = ["self"]
-        mapping = {}
-        for _property in properties_by_domain.get(step_class["@id"]):
+        args = ast.arguments(
+            args=[ast.arg(arg="self", annotation=None)],
+            vararg=None,
+            kwonlyargs=None,
+            kw_defaults=None,
+            kwarg=None,
+            defaults=[],
+        )
+        name_to_property_name: Dict[str, ast.Str] = {}
+        for _property in properties_by_domain[step_class["@id"]]:
             argument_name = remove_linked_ql(_property["@id"])
             if argument_name == "from":
                 continue
-            mapping[_property["@id"]] = argument_name
+            name_to_property_name[argument_name] = ast.Str(_property["@id"])
             _type = range_to_type(_property["rdfs:range"])
             if _property["@id"] not in single_properties:
-                _type = "typing.List[" + _type + "]"
-            arguments.append(f"{argument_name}: {_type}")
-        body = ",\n".join(
-            '            "' + key + '": ' + value
-            for key, value in {
-                "@type": '"' + step_class["@id"] + '"',
-                **mapping,
-            }.items()
+                _type = ast.Subscript(
+                    value=ast.Attribute(value=ast.Name(id="typing"), attr="List"),
+                    slice=ast.Index(value=_type),
+                )
+            args.args.append(ast.arg(arg=argument_name, annotation=_type))
+        keys = [name_to_property_name[arg.arg] for arg in args.args[1:]]
+        values = [ast.Name(arg.arg) for arg in args.args[1:]]
+        step_dict = ast.Dict(
+            keys=[ast.Str("@type"), *keys], values=[ast.Str(step_class["@id"]), *values]
         )
-        method_str += (
-            ", ".join(arguments)
-            + f""") -> 'Path':
-        self.__add_step({{
-{body}
-        }})
-        return self
-"""
+        function_def = ast.FunctionDef(
+            name=method_name,
+            args=args,
+            returns=ast.Str(s="Path"),
+            decorator_list=[],
+            body=[
+                ast.Expr(
+                    value=ast.Call(
+                        func=ast.Attribute(
+                            value=ast.Name(id="self"), attr="__add_step"
+                        ),
+                        args=[step_dict],
+                        keywords=[],
+                    )
+                ),
+                ast.Return(value=ast.Name(id="self")),
+            ],
         )
-        text += "    " + method_str + "\n"
-    return text
+        class_def.body.append(function_def)
+    return astor.to_source(module)
